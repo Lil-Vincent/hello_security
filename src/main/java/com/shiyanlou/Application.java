a33a157a00d810a2f0a353889a7a9985da3a7099
Lil <1378951829@qq.com>
2021-11-05 11:01:04 +0800

    Initial commit

---
+++ src/main/java/com/shiyanlou/Application.java
@@ -0,0 +1,15 @@
+package com.shiyanlou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring boot 项目启动类
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/shiyanlou/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.shiyanlou;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * spring mvc 视图控制器
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
+
+
+
